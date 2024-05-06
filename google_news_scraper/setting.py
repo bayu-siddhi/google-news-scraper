33f6246564b07d16e3ec8cd66e577e21dba26dd7
Bayu Siddhi Mukti <bayu2mukti18@gmail.com>
2024-05-06 23:30:44 +0700

    Add setting.py

---
+++ google_news_scraper/setting.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+
+def chrome_option() -> webdriver.ChromeOptions:
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    return chrome_options
