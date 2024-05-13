cf162d43a66f0ad1216d76340272b75c89296be2
Bayu Siddhi Mukti <bayu2mukti18@gmail.com>
2024-05-13 14:55:24 +0700

    Update setting.py for testing

--- google_news_scraper/setting.py
+++ google_news_scraper/setting.py
@@ -13,7 +13,9 @@ def chrome_option() -> webdriver.ChromeOptions:
         selenium.webdriver.ChromeOptions
     """    
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
+    # chrome_options.add_argument('--headless')
     chrome_options.add_argument('--no-sandbox')
+    # chrome_options.add_argument('--disable-gpu')
+    # chrome_options.add_argument('--log-level=1')
     chrome_options.add_argument('--disable-dev-shm-usage')
     return chrome_options
