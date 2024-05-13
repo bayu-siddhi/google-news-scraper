6c51519937a5b3c397245f99d51a776cff7f0501
Bayu Siddhi Mukti <bayu2mukti18@gmail.com>
2024-05-13 15:02:52 +0700

    Update setting.py for testing

--- google_news_scraper/setting.py
+++ google_news_scraper/setting.py
@@ -13,7 +13,17 @@ def chrome_option() -> webdriver.ChromeOptions:
         selenium.webdriver.ChromeOptions
     """    
     chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument('--headless')
+
+    IN_COLAB = False
+    try:
+        import google.colab
+        IN_COLAB = True
+    except Exception as e:
+        pass
+
+    if IN_COLAB:
+        chrome_options.add_argument('--headless')
+    
     chrome_options.add_argument('--no-sandbox')
     # chrome_options.add_argument('--disable-gpu')
     # chrome_options.add_argument('--log-level=1')
